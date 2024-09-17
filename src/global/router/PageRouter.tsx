e8d25ab925c4383f785ffb9850240cca7a40cf52
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-09-17 21:49:21 +0900

    update globals

--- src/global/router/PageRouter.tsx
+++ src/global/router/PageRouter.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route } from "@solidjs/router";
 import Dashboard from "pages/dashboard/Dashboard";
 import Home from "pages/home/Home";
 import Login from "pages/login/Login";
+import Record from "pages/record/Record";
 import Register from "pages/register/Register";
 
 export default () => {
@@ -10,8 +11,9 @@ export default () => {
             <Route path={"/"} component={() => <Navigate href={"/login"} />} />
             <Route path={"/login"} component={Login} />
             <Route path={"/register"} component={Register} />
-            <Route path={["/home","/~"]} component={Home}>
-                <Route path={["/","/dashboard"]} component={Dashboard} />
+            <Route path={["/home", "/~"]} component={Home}>
+                <Route path={["/", "/dashboard"]} component={Dashboard} />
+                <Route path={"/record"} component={Record} />
             </Route>
         </>
     )

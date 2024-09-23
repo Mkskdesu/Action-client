73a9e90c14b489195ac290e69f11ba19860779e4
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-09-23 23:56:02 +0900

    add types

---
+++ src/global/types/user.d.ts
@@ -0,0 +1,28 @@
+interface loginResponse {
+    successful: boolean,
+    data: {
+        uuid: string,
+        userid: string,
+        sync: {
+            time: number
+            device: string
+        },
+        user: apiUserData
+    }
+}
+
+interface apiUserData {
+    name: string
+    icon: string,
+    createdAt: number
+    social: {
+        follow: Array<string>
+        follower: Array<string>
+    }
+    record: {}
+    notification: {
+        priority: Array<notification>
+        normal: Array<notification>
+        untagged: Array<notification>
+    }
+}

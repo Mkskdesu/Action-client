0e107195a5f60e926e8cc8eb0ce5e3101d3017df
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-11-25 01:29:01 +0900

    update afterlogin

--- src/features/afterLogin/afterLogin.ts
+++ src/features/afterLogin/afterLogin.ts
@@ -1,5 +1,13 @@
-
+import defaultUser from "@/features/localStorage/defaults/user.ts";
 
 export function setData(data:loginResponse) {
+    const userStorageData: typeof defaultUser= JSON.parse(localStorage.getItem("user")|| "{}");
+    if(data.successful){
+        userStorageData.name = data.data.user.name;
+        userStorageData.uuid = data.data.uuid;
+        userStorageData.createdAt = data.data.user.createdAt;
+        localStorage.setItem("user", JSON.stringify(userStorageData));
+    }
+    location.pathname = "/";
     
 }

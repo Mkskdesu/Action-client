b39c9562a9381dfe888fcbf092394fe49e477aa6
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-11-25 01:28:50 +0900

    update ai

--- src/features/aiSession/aiSession.ts
+++ src/features/aiSession/aiSession.ts
@@ -1,3 +1,5 @@
+import {baseUrl} from "global/constants/baseUrl.ts";
+
 interface aiSessionOptions {
     mode: "edge" | "server" | "auto",
 }
@@ -8,9 +10,24 @@ async function checkEdgeAIAvaliable() {
     else return true;
 }
 
+interface log {
+    sysPrompt: string;
+    userPrompt: string;
+    result: aiResult
+}
+
+interface aiResult {
+
+    role: string;
+    content: string;
+    refusal: null
+
+}
+
 export default class aiSession {
     mode = "auto";
     ready = false;
+    logs: Array<log> = []
     constructor(options: aiSessionOptions) {
         if (options.mode != "server") {
             checkEdgeAIAvaliable().then((r)=>this.decideMode(options.mode,r));
@@ -27,6 +44,24 @@ export default class aiSession {
     }
     createSession(){
         
+    }
+    async textPrompt(sysPrompt: string,userPrompt: string){
+        const url = new URL(baseUrl)
+        url.pathname = "/api/ai";
+        const result = await fetch(url,{
+            method: "POST",
+            headers: {
+                "Origin": location.hostname,
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({sysPrompt, userPrompt}),
+        }).then(res=> {
+            if(res.ok) return res.json()
+        }).then(json=>json.text as aiResult)
+            .catch(console.error);
+        if(result)
+            this.logs.push({sysPrompt,userPrompt,result});
+        return result;
     }
 
 }

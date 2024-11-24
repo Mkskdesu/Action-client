9fe814955ff6bcbc996899914060716a13df31c5
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-11-25 01:28:43 +0900

    update login

--- src/features/login/login.ts
+++ src/features/login/login.ts
@@ -1,6 +1,10 @@
 import { baseUrl } from "global/constants/baseUrl"
 import { setData } from "../afterLogin/afterLogin";
 
+import defaultUser from "@/features/localStorage/defaults/user";
+
+
+
 async function judgeResult(data: Response) {
     const json = await data.json();
     if (!data.ok || !json.successful) {
@@ -14,12 +18,14 @@ export const autoLogin = () => {
     return new Promise((resolve, reject) => {
         const url = new URL(baseUrl)
         url.pathname = "/users/autologin";
+        const storageData: typeof defaultUser = JSON.parse(localStorage.getItem("user")|| "{}");
         fetch(url, {
             method: "POST",
             headers: {
-                "Origin": location.hostname
+                "Origin": location.hostname,
+                "content-type": "application/json",
             },
-            body: JSON.stringify({ uuid: JSON.parse(localStorage.getItem("user") || "{}").uuid || "" })
+            body: JSON.stringify({ uuid: JSON.parse(localStorage.getItem("user") || "{}").uuid || "", deviceid:  storageData.device.id})
         }).then(judgeResult)
             .then(setData)
             .then(resolve)
@@ -31,11 +37,13 @@ export const login = (id: string, password: string) => {
     return new Promise((resolve, reject) => {
         const url = new URL(baseUrl);
         url.pathname = "/users/login";
+        const storageData: typeof defaultUser = JSON.parse(localStorage.getItem("user")|| "{}");
         fetch(url, {
             method: "POST",
             headers: {
-                "Origin": location.hostname
-            }, body: JSON.stringify({ userid: id, password: password })
+                "Origin": location.hostname,
+                "content-type": "application/json",
+            }, body: JSON.stringify({ loginid: id, password: password ,deviceid:  storageData.device.id})
         }).then(judgeResult)
             .then(setData)
             .then(resolve)

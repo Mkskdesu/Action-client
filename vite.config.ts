5b030bfd26ecb1fdc05240d292d9d5dca2c348d3
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-12-12 17:02:20 +0900

    add google tag manager

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,31 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import solid from 'vite-plugin-solid';
 import devtools from "solid-devtools/vite";
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 export default defineConfig({
-  plugins: [solid(),
-  devtools({
-    autoname: true,
-    locator: {
-      targetIDE: 'vscode',
-      componentLocation: true,
-      jsxLocation: true,
+    plugins: [solid(),
+        devtools({
+            autoname: true,
+            locator: {
+                targetIDE: 'vscode',
+                componentLocation: true,
+                jsxLocation: true,
+            }
+        }),
+        VitePluginRadar({
+            // Google Analytics tag injection
+            analytics: {
+                id: import.meta.env.GTAG,
+            },
+        })
+    ],
+    resolve: {
+        alias: {
+            "global/": "/src/global/",
+            "pages/": "/src/pages/",
+            "assets/": "/src/assets/",
+            "@/": "/src/"
+        }
     }
-  })
-  ],
-  resolve: {
-    alias: {
-      "global/": "/src/global/",
-      "pages/": "/src/pages/",
-      "assets/": "/src/assets/",
-      "@/": "/src/"
-    }
-  }
 })

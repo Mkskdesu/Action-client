320728930f8c3cc418174d0298f9266f70ac26a6
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-12-12 22:43:53 +0900

    add ai features to calendar

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, onMount } from "solid-js";
+import {createEffect, createSignal, on, onMount, Show} from "solid-js";
 import { CgChevronLeft, CgChevronRight } from "solid-icons/cg";
 import clsx from "clsx";
 import dayjs from "dayjs";
@@ -20,6 +20,10 @@ import loginStreakPrompt from "pages/dashboard/aiPrompt/loginStreakPrompt.ts";
 import {loginStreak} from "global/states/loginStreak.ts";
 import TypeWriter from "global/components/typewriter/TypeWriter.tsx";
 import AsyncTypeWriter from "global/components/typewriter/AsyncTypeWriter.tsx";
+import {A} from "@solidjs/router";
+import getRecords from "global/components/grassCalendar/getRecords.ts";
+import sumArray from "global/utils/sumArray.ts";
+import grassCalendarPrompt from "pages/dashboard/aiPrompt/grassCalendarPrompt.ts";
 
 export default () => {
 
@@ -27,8 +31,10 @@ export default () => {
 
     const [weekBase, setWeekBase] = createSignal(dayjs().startOf("week"));
     const [monthBase, setMonthBase] = createSignal(dayjs().startOf("month"));
+    const [monthTotal,setMonthTotal] = createSignal([0,0]);
     const [graphUnit, setGraphUnit] = createSignal(false);
     const [loginCounterText,setLoginCounterText] = createSignal("");
+    const [grassCalendarText,setGrassCalendarText] = createSignal("");
 
     const ai = new aiSession({mode:"server"});
     let streak;
@@ -38,22 +44,43 @@ export default () => {
         setBottomBarState("home");
         ai.createSession();
         generateStreakText();
+        generateGrassCalendarText()
     });
     
+    createEffect(on(monthBase,()=>{
+        let time = getTotalMonthTime();
+        const hour = Math.floor(time/60);
+        time -= hour*60;
+        setMonthTotal([hour,time]);
+    }))
+    
     function generateStreakText() {
-        const result = ai.textPrompt(loginStreakPrompt.sysPrompt,loginStreakPrompt.userPrompt + JSON.stringify({streak:loginStreak()[0], diff:loginStreak()[1], lastLogin: loginStreak()[2]})).then(r=>r?.content||"").then(setLoginCounterText)
+        const result = ai.textPrompt(loginStreakPrompt.sysPrompt,loginStreakPrompt.userPrompt + JSON.stringify({streak:loginStreak()[0], diff:loginStreak()[1], lastLogin: loginStreak()[2]}))
+            .then(r=>r?.content||"").then(setLoginCounterText)
+    }
+    function generateGrassCalendarText() {
+        const result = ai.textPrompt(grassCalendarPrompt.sysPrompt,grassCalendarPrompt.userPrompt + JSON.stringify({targetMonth:monthBase().month(), record: JSON.parse(localStorage.getItem("record")||"{}") }))
+            .then(r=>r?.content||"").then(setGrassCalendarText);
         console.log(result);
+    }
+    
+    function getTotalMonthTime(){
+        return sumArray(getRecords(monthBase()))
     }
 
     return (
         <div class={style.dashboard}>
+            <div class={style.notification}>
+                [お知らせ] 12月12日 23:00更新 <span style={{"font-family": "Kamaboko"}}>ACTION</span> Ver.0.1.1
+                アップデート配信! <A href={"patchnote"}>更新内容とパッチノートはこちら</A>
+            </div>
             <div class={style.loginCounter}>
                 <TypeWriter content={`現在 ${(loginStreak()[0]).toString()} 日連続ログイン中! `} />
                 <TypeWriter content={loginCounterText()} />
             </div>
             <div class={style.grassCalendar}>
                 <div class={style.title}>
-                    <h2>{monthBase().year()} 年 {monthBase().month() + 1} 月</h2>
+                    <h2>{monthBase().year()} 年 {monthBase().month() + 1} 月</h2> <p>総学習時間 : {monthTotal()[0]}時間{monthTotal()[1]}分</p>
                     <div class={style.spacer}></div>
                     <IconTextButton class={style.button} icon={<CgChevronLeft/>}
                                     onClick={() => setMonthBase(p => p.subtract(1, "month"))}>
@@ -65,11 +92,14 @@ export default () => {
                     </IconTextButton>
                     <button class={clsx(style.button, style.detailsbutton)}>詳細</button>
                 </div>
+                    <div class={style.textArea}>
+                        <TypeWriter content={grassCalendarText()}/>
+                    </div>
                 <GrassCalendar {...{monthBase}} />
             </div>
             <div class={style.weekCalendar}>
-                <div class={style.title}>
-                    <h2>今週の概要 - 第 {weekBase().week()} 週</h2>
+            <div class={style.title}>
+                    <h2>一週間の概要 - 第 {weekBase().week()} 週</h2>
                     <div class={style.spacer}></div>
                     <IconTextButton class={style.button} icon={<CgChevronLeft/>}
                                     onClick={() => setWeekBase(p => p.subtract(1, "week"))}>

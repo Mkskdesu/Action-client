2d6e8781c4ed505456ee8076b1ff7b0bf0601bff
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-12-12 22:39:23 +0900

    update loginstreak

--- src/features/loginStreak/checkStreak.ts
+++ src/features/loginStreak/checkStreak.ts
@@ -4,19 +4,21 @@ import dayjs from "dayjs";
 export default () => {
     const storageData: typeof achievements = JSON.parse(localStorage.getItem("achievements") || "{}");
     const previousData = storageData.loginStreak.lastLogin;
-    const diff = dayjs().startOf("day").diff(dayjs(storageData.loginStreak.lastLogin).startOf("day"),"day");
-    if (diff == 0) {
-        return [storageData.loginStreak.streak,diff,previousData];
-    }else if (diff == 1) {
+    const dayDiff = dayjs().startOf("day").diff(dayjs(previousData).startOf("day"),"day");
+    const diffValue = dayjs().diff(dayjs(previousData),"minute");
+    console.log("diff:",dayDiff);
+    if (dayDiff == 0) {
+        return [storageData.loginStreak.streak,diffValue,previousData];
+    }else if (dayDiff == 1) {
         storageData.loginStreak.streak++;
         storageData.loginStreak.lastLogin = dayjs().startOf("day").valueOf();
         localStorage.setItem("achievements", JSON.stringify(storageData));
-        return [storageData.loginStreak.streak,diff,previousData];
+        return [storageData.loginStreak.streak,diffValue,previousData];
     }else{
-        storageData.loginStreak.streak = 0;
+        storageData.loginStreak.streak = 1;
         storageData.loginStreak.lastLogin = dayjs().startOf("day").valueOf();
         localStorage.setItem("achievements", JSON.stringify(storageData));
-        return [storageData.loginStreak.streak,diff,previousData];
+        return [storageData.loginStreak.streak,diffValue,previousData];
     }
     
 

9da9404134df4c29cbcd553eea9618909ea82194
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2024-12-15 14:21:17 +0900

    fix selectbox

--- src/global/components/selectbox/normalSelectBox/NormalSelectBox.tsx
+++ src/global/components/selectbox/normalSelectBox/NormalSelectBox.tsx
@@ -1,80 +1,66 @@
 import * as solid from "solid-js";
-import { BsCaretDownFill } from "solid-icons/bs";
+import { BsCaretDownFill } from 'solid-icons/bs'
+
+import style from "./NormalSelectBox.module.scss";
 import clsx from "clsx";
-import { Transition, TransitionGroup } from "solid-transition-group";
+import {createSignal} from "solid-js";
 
-import style from './NormalSelectBox.module.scss';
-import hotkeys from "hotkeys-js";
-
-
-
-
-interface selectBoxPropsType extends propsType {
-    contents: Array<selectContents>
-    onChange?: FunctionWithType<selectContents["value"]>
-    value: selectContents
+interface normalSelectBoxProps {
+    class?: string
+    value: { label: solid.JSXElement, value:any }
+    contents: Array<{ label: solid.JSXElement, value:any }>
+    onChange?: (e:any)=>void
 }
 
 interface optionProps {
-    changeValue: FunctionWithType<MouseEvent & { target: Element, currentTarget: HTMLButtonElement }, any>
+    onClick?: (e:any) => void
+    label: solid.JSXElement 
     value: any
-    label: string
 }
 
 
-const SelectBox: solid.Component<selectBoxPropsType> = (props) => {
-    const [value, setValue] = solid.createSignal<any>(props.value.value);
-    const [label, setLabel] = solid.createSignal<solid.JSXElement>(props.value.label);
+export default(props:normalSelectBoxProps) => {
 
-    let wrapperRef: HTMLDivElement | undefined;
+    let labelRef: HTMLButtonElement | undefined;
+    
+    const [label,setLabel] = createSignal(props.value.label||props.contents[0].label);
 
-    solid.onMount(() => {
-        hotkeys("Esc", () => wrapperRef?.blur());
-    });
-
-    solid.onCleanup(() => {
-        hotkeys.unbind();
-    });
-
-    function changeValue(e: MouseEvent & { target: Element, currentTarget: HTMLButtonElement }, value: any) {
-        setValue(value);
-        setLabel(props.contents.find(v => v.value == value)?.label || "");
-        if (props.onChange)
-            props.onChange(value);
-
-        wrapperRef?.blur();
+    
+    function handleChange(e:{label:string,value:any}) {
+        setLabel(e.label);
+        props.onChange && props.onChange(e.value);
     }
 
-
     return (
-        <div class={clsx(style.selectBox, props.class)} ref={wrapperRef}>
-            <button class={clsx(style.label)}>
+        <div class={clsx(style.selectBox, props.class)} tabIndex={0}>
+            <button class={clsx(style.label)} ref={labelRef} tabIndex={0} onClick={e => e.currentTarget.focus()}>
                 <span>{label()}</span>
                 <BsCaretDownFill />
             </button>
 
             <div class={clsx(style.selector)}>
                 <solid.For each={props.contents}>
-                    {({ value, label }) => <Selector {...{ changeValue, value, label }} />}
+                    {({ value, label }) => <Selector {...{ value, label }} onClick={handleChange} />}
                 </solid.For>
             </div>
         </div>
     )
 }
 
-
 const Selector: solid.Component<optionProps> = (props) => {
     let optionRef: HTMLButtonElement | undefined
 
     function handleChange(e: MouseEvent & { target: Element, currentTarget: HTMLButtonElement }) {
-        props.changeValue(e, props.value);
+        e.currentTarget.focus();
+        //if (props.onClick) props.onClick(props.value);
+        props.onClick && props.onClick({label:props.label,value:props.value})
         e.currentTarget.blur();
     }
 
     return (
-        <button class={style.options} onClick={handleChange} ref={optionRef}>
+        <button class={style.options} onClick={handleChange} ref={optionRef} tabIndex={0} onPointerDown={e=>e.currentTarget.focus()}>
             {props.label}
         </button>
     )
 }
-export default SelectBox;
+

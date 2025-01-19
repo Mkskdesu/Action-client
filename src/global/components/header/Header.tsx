0939b1b70a16b30144460959ac23435a8922f530
= <=>
2025-01-19 17:05:22 +0900

    update header

--- src/global/components/header/Header.tsx
+++ src/global/components/header/Header.tsx
@@ -12,12 +12,15 @@ import {Match, Show, Switch} from "solid-js";
 import {countDownTimerState, countUpTimerState} from "global/states/timerState.ts";
 import TimerBadge from "global/components/header/timerBadge/TimerBadge.tsx";
 import {Transition} from "solid-transition-group";
+import {useNavigate} from "@solidjs/router";
 
 
 export default () => {
 
     const storageData: typeof defaultUser = JSON.parse(localStorage.getItem("user")|| "{}");
     let headerRef: HTMLElement|undefined;
+    
+    const navigate = useNavigate();
     
     function enter(e:Element,done:()=>void):void {
         headerRef?.classList.add(style.enter);
@@ -28,10 +31,13 @@ export default () => {
         done()
     }
     
+    function handleLogoClick(){
+        navigate("/~/")
+    }
     return (
         
             <header class={style.heading} ref={headerRef}>
-                <span class={style.title}>Action</span>
+                <span class={style.title} onClick={handleLogoClick}>Action</span>
                 <span class={style.pageTitle}>
                 {pageTitle()}
             </span>
